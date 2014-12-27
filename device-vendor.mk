85afd74daa93db5040411f821fb400c7ad253967
GeyerA <calo.geyer@gmail.com>
2014-12-27 11:30:12 +0100

    Initial bringup

--- device-vendor.mk
+++ device-vendor.mk
@@ -29,4 +29,4 @@ PRODUCT_COPY_FILES := \
 # Pick up overlay for features that depend on non-open-source files
 DEVICE_PACKAGE_OVERLAYS := vendor/asus/flo/overlay
 
-$(call inherit-product, vendor/asus/flo/flo-vendor-blobs.mk)
+$(call inherit-product, vendor/asus/flo/device-partial.mk)
